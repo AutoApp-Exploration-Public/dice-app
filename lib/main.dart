82cd7b2d479f0d7049dfc09fce212c652af82694
thetechgirlgita <thetechgirlgita@gmail.com>
2020-08-02 01:02:41 -0400

    Dice an application of two dices

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.redAccent,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.redAccent,
         ),
         body: DicePage(),
       ),
@@ -15,9 +16,43 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int LeftDiceNumber = 1;
+  int RightDiceNumber = 1;
+  void dicechanger() {
+    setState(() {
+      RightDiceNumber = Random().nextInt(6) + 1;
+      LeftDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                dicechanger();
+              },
+              child: Image.asset('images/dice$LeftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                dicechanger();
+              },
+              child: Image.asset('images/dice$RightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
